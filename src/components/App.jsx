37658d8fb41e19f1b727e88e633c42a997703776
MaccKOT <51321216+MaccKOT@users.noreply.github.com>
2020-07-16 17:46:16 +0400

    All components works fine

---
+++ src/components/App.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import './App.css';
+import Button from '@material-ui/core/Button';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.jsx</code> and SAVE to reload.
+        </p>
+        <Button variant="contained" color="primary">
+          Material UI is working fine!
+        </Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
